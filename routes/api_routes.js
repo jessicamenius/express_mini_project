1422ca9c10d8bcb7f6cb90610b4a9d02d556b5a7
Jessica <jessicamenius@gmail.com>
2020-06-13 13:17:16 -0500

    adding pokemon data

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
+const util = require("util");
+
+const readFileSync = util.promisify(fs.readFile);
+const writeFileSync = util.promisify(fs.writeFIle);
 
 router.get("/api", (req, res) => {
   res.json({ msg: "success" });
 });
+
+//api/all
+
+router.get("/api/all", async (req, res) => {
+  res.json({ msg: "success" });
+  let pokemonData = await readFileSync("data.json");
+  pokemonData = JSON.parse(pokemonData);
+  res.send(pokemonData);
+});
+
+//route/get
+
+//use fs read json files
+
+//convert
+
+//send the enitre array in res.json
 
 module.exports = router;
